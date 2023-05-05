6265f9727fc8a900e39a7dabc5f783c9bc26a4f4
Vansh <vansh432.sharma@gmail.com>
2023-05-05 23:18:09 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import React , { useState } from 'react';
 import './App.css';
 import PhoneVerificationPopup from './Phone';
 function App() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleClick = () => {
+    setShowPopup(true);
+  }
+
   return (
     <div className="App">
-     <PhoneVerificationPopup />
+      <button className="popup-button" onClick={handleClick}>Verify Phone</button>
+      {showPopup && <PhoneVerificationPopup onClose={() => setShowPopup(false)} />}
     </div>
   );
 }
